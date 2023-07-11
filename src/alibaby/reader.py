33c4183600871857554c69cee7750df378e29915
richard gowers <richardjgowers@gmail.com>
2023-07-11 10:34:48 +0100

    working traj reader

--- src/alibaby/reader.py
+++ src/alibaby/reader.py
@@ -8,7 +8,7 @@ class FEReader(ReaderBase):
     """A MDAnalysis Reader for nc files created by openfe RFE Protocol"""
     _state_id: int
     _frame_index: int
-    _dataset
+    _dataset: nc.Dataset
 
     format = 'openfe RFE'
 
@@ -26,11 +26,16 @@ class FEReader(ReaderBase):
         except KeyError:
             raise ValueError("Specify the state_id")
 
-        super().__init__(filename, convert_units, kwargs)
+        super().__init__(filename, convert_units, **kwargs)
 
         self._dataset = nc.Dataset(filename)
-        self._n_atoms = ds.dimensions['atom'].size
+        self._n_atoms = self._dataset.dimensions['atom'].size
+        self.ts = Timestep(self._n_atoms)
         self._read_frame(0)
+
+    @property
+    def n_atoms(self) -> int:
+        return self._n_atoms
 
     @property
     def n_frames(self) -> int:
@@ -44,12 +49,12 @@ class FEReader(ReaderBase):
         return n_atoms
 
     def _read_next_timestep(self, ts=None) -> Timestep:
+        if (self._frame_index + 1) >= len(self):
+            raise EOFError
         return self._read_frame(self._frame_index + 1)
 
     def _read_frame(self, frame: int) -> Timestep:
         self._frame_index = frame
-
-        ts = Timestep(self._n_atoms)
 
         pos = handle_trajectories._state_positions_at_frame(
             self._dataset,
@@ -60,11 +65,11 @@ class FEReader(ReaderBase):
             self._state_id,
             self._frame_index)
 
-        ts.positions = (pos.to(unit.angstrom)).m
-        ts.dimensions = dim
-        ts.frame = self._frame_index
+        self.ts.positions = (pos.to(unit.angstrom)).m
+        self.ts.dimensions = dim
+        self.ts.frame = self._frame_index
 
-        return ts
+        return self.ts
 
     def _reopen(self):
         self._frame_index = -1
