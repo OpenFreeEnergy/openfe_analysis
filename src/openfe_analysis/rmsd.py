f326d07bbed5b4f23da56f896b6dd273029ffaec
richard gowers <richardjgowers@gmail.com>
2023-09-07 14:25:16 +0100

    add 2D RMSD

--- src/openfe_analysis/rmsd.py
+++ src/openfe_analysis/rmsd.py
@@ -1,3 +1,4 @@
+import itertools
 import MDAnalysis as mda
 from MDAnalysis.analysis import rms
 import netCDF4 as nc
@@ -52,6 +53,7 @@ def gather_rms_data(pdb_topology, dataset):
         'protein_RMSD': [],
         'ligand_RMSD': [],
         'ligand_wander': [],
+        'protein_2D_RMSD': [],
     }
 
     ds = nc.Dataset(dataset)
@@ -61,6 +63,10 @@ def gather_rms_data(pdb_topology, dataset):
 
         prot = u.select_atoms('protein and name CA')
         ligand = u.select_atoms('resname UNK')
+
+        # save coordinates for 2D RMSD matrix
+        # TODO: Some smart guard to avoid allocating a silly amount of memory?
+        prot2d = np.empty((len(u.trajectory), len(prot), 3), dtype=np.float32)
 
         prot_start = prot.positions
         prot_weights = prot.masses / np.mean(prot.masses)
@@ -74,6 +80,7 @@ def gather_rms_data(pdb_topology, dataset):
 
         for ts in u.trajectory:
             if prot:
+                prot2d[ts.frame, :, :] = prot.positions
                 this_protein_rmsd.append(
                     rms.rmsd(prot.positions, prot_start, prot_weights,
                              center=False, superposition=False)
@@ -88,11 +95,43 @@ def gather_rms_data(pdb_topology, dataset):
                 )
 
         if prot:
+            rmsd2d = twoD_RMSD(prot2d, prot_weights)
             output['protein_RMSD'].append(this_protein_rmsd)
+            output['protein_2D_RMSD'].append(rmsd2d)
         if ligand:
             output['ligand_RMSD'].append(this_ligand_rmsd)
             output['ligand_wander'].append(this_ligand_wander)
 
     output['time(ps)'] = list(np.arange(len(u.trajectory)) * u.trajectory.dt)
+
+    return output
+
+
+def twoD_RMSD(positions, w) -> list[float]:
+    """2 dimensions RMSD
+
+    Parameters
+    ----------
+    positions : np.ndarray
+      the protein positions for the entire trajectory
+    w : np.ndarray
+      weights array
+
+    Returns
+    -------
+    rmsd_matrix : list
+      a flattened version of the 2d
+    """
+    nframes, _, _ = positions.shape
+
+    output = []
+
+    for i, j in itertools.combinations(range(nframes), 2):
+        posi, posj = positions[i], positions[j]
+
+        rmsd = rms.rmsd(posi, posj, w,
+                        center=True, superposition=True)
+
+        output.append(rmsd)
 
     return output
