75d8af5d5dc4bb59eb0be0c19af470b09e872e48
richard gowers <richardjgowers@gmail.com>
2023-11-21 13:01:27 +0000

    require output directory for RFE_analysis command

--- src/openfe_analysis/cli.py
+++ src/openfe_analysis/cli.py
@@ -16,9 +16,14 @@ def cli():
                                        file_okay=False,
                                        dir_okay=True,
                                        path_type=pathlib.Path))
-def rfe_analysis(loc):
+@click.argument('output', type=click.Path(writable=True,
+                                          dir_okay=False,
+                                          path_type=pathlib.Path))
+def rfe_analysis(loc, output):
     pdb = loc / "hybrid_system.pdb"
     trj = loc / "simulation.nc"
 
     data = rmsd.gather_rms_data(pdb, trj)
-    click.echo(json.dumps(data))
+
+    with click.open_file(output, 'w') as f:
+        f.write(json.dumps(data))
