331b47e08403ad0f6f48189d8c27a42fc73e1b79
hannahbaumann <hbaumann@uci.edu>
2025-01-23 13:33:14 +0100

    Switch to new results

--- src/openfe_analysis/tests/conftest.py
+++ src/openfe_analysis/tests/conftest.py
@@ -3,14 +3,27 @@ import pooch
 import pytest
 
 
+# RFE_OUTPUT = pooch.create(
+#     path=pooch.os_cache("openfe_analysis"),
+#     base_url="doi:10.6084/m9.figshare.24101655",
+#     registry={
+#         "checkpoint.nc": "5af398cb14340fddf7492114998b244424b6c3f4514b2e07e4bd411484c08464",
+#         "db.json": "b671f9eb4daf9853f3e1645f9fd7c18150fd2a9bf17c18f23c5cf0c9fd5ca5b3",
+#         "hybrid_system.pdb": "07203679cb14b840b36e4320484df2360f45e323faadb02d6eacac244fddd517",
+#         "simulation.nc": "92361a0864d4359a75399470135f56642b72c605069a4c33dbc4be6f91f28b31",
+#         "simulation_real_time_analysis.yaml": "65706002f371fafba96037f29b054fd7e050e442915205df88567f48f5e5e1cf",
+#     }
+# )
+
+
 RFE_OUTPUT = pooch.create(
     path=pooch.os_cache("openfe_analysis"),
-    base_url="doi:10.6084/m9.figshare.24101655",
+    base_url="doi:10.6084/m9.figshare.28263203",
     registry={
         "checkpoint.nc": "5af398cb14340fddf7492114998b244424b6c3f4514b2e07e4bd411484c08464",
         "db.json": "b671f9eb4daf9853f3e1645f9fd7c18150fd2a9bf17c18f23c5cf0c9fd5ca5b3",
-        "hybrid_system.pdb": "07203679cb14b840b36e4320484df2360f45e323faadb02d6eacac244fddd517",
-        "simulation.nc": "92361a0864d4359a75399470135f56642b72c605069a4c33dbc4be6f91f28b31",
+        "hybrid_system.pdb": "77c7914b78724e568f38d5a308d36923f5837c03a1d094e26320b20aeec65fee",
+        "simulation.nc": "6749e2c895f16b7e4eba196261c34756a0a062741d36cc74925676b91a36d0cd",
         "simulation_real_time_analysis.yaml": "65706002f371fafba96037f29b054fd7e050e442915205df88567f48f5e5e1cf",
     }
 )
