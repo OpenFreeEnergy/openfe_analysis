8ba8087a08247a3a881e94e65d9260611c2687c9
hannahbaumann <hbaumann@uci.edu>
2026-01-16 17:10:24 +0100

    Try removing locking

--- src/openfe_analysis/tests/conftest.py
+++ src/openfe_analysis/tests/conftest.py
@@ -3,12 +3,9 @@ from importlib import resources
 import pathlib
 import pooch
 import pytest
-from filelock import FileLock
 
 POOCH_CACHE = pooch.os_cache("openfe_analysis")
 POOCH_CACHE.mkdir(parents=True, exist_ok=True)
-LOCKFILE = POOCH_CACHE / "prepare.lock"
-READY_FLAG = POOCH_CACHE / "data_ready.flag"
 
 ZENODO_RBFE_DATA = pooch.create(
     path=POOCH_CACHE,
@@ -19,45 +16,24 @@ ZENODO_RBFE_DATA = pooch.create(
     },
 )
 
-def _prepare_data():
-    """Download and extract large test data once per machine."""
-    if READY_FLAG.exists():
-        return
+def _fetch_and_untar_once(filename: str) -> pathlib.Path:
+    untar_dir = POOCH_CACHE / f"{filename}.untar"
 
-    with FileLock(str(LOCKFILE)):
-        if READY_FLAG.exists():
-            return
+    if not untar_dir.exists():
+        ZENODO_RBFE_DATA.fetch(filename, processor=pooch.Untar())
 
-        ZENODO_RBFE_DATA.fetch(
-            "openfe_analysis_simulation_output.tar.gz",
-            processor=pooch.Untar(),
-        )
-        ZENODO_RBFE_DATA.fetch(
-            "openfe_analysis_skipped.tar.gz",
-            processor=pooch.Untar(),
-        )
-
-        READY_FLAG.touch()
-
-
-_prepare_data()
+    return untar_dir
 
 @pytest.fixture(scope="session")
 def rbfe_output_data_dir() -> pathlib.Path:
-    return (
-        POOCH_CACHE
-        / "openfe_analysis_simulation_output.tar.gz.untar"
-        / "openfe_analysis_simulation_output"
-    )
+    untar_dir = _fetch_and_untar_once("openfe_analysis_simulation_output.tar.gz")
+    return untar_dir / "openfe_analysis_simulation_output"
 
 
 @pytest.fixture(scope="session")
 def rbfe_skipped_data_dir() -> pathlib.Path:
-    return (
-        POOCH_CACHE
-        / "openfe_analysis_skipped.tar.gz.untar"
-        / "openfe_analysis_skipped"
-    )
+    untar_dir = _fetch_and_untar_once("openfe_analysis_skipped.tar.gz")
+    return untar_dir / "openfe_analysis_skipped"
 
 @pytest.fixture(scope="session")
 def simulation_nc(rbfe_output_data_dir) -> pathlib.Path:
