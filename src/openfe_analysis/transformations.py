6c1a6c5d7651e7c0cdbd5666cef8f012ce698be3
Hannah Baumann <43765638+hannahbaumann@users.noreply.github.com>
2026-02-02 09:02:32 +0100

    Apply suggestion from @hannahbaumann

--- src/openfe_analysis/transformations.py
+++ src/openfe_analysis/transformations.py
@@ -64,7 +64,7 @@ class ClosestImageShift(TransformationBase):
         for ag in self.targets:
             vec = ag.center_of_mass() - center
             frac = np.linalg.solve(box.T, vec)  # fractional coordinates
-            shift = np.dot(np.round(frac), box)  # nearest image
+            shift = np.dot(np.round(frac), box)  # nearest image, then compute shift
             ag.positions -= shift
 
         return ts
