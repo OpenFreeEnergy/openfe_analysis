48dc08eda167dd7c54cf4b69d63e53700083fb98
hannahbaumann <hbaumann@uci.edu>
2026-08-12 14:45:17 +0200

    Update tests

--- src/openfe_analysis/tests/utils/test_apply_transformations.py
+++ src/openfe_analysis/tests/utils/test_apply_transformations.py
@@ -25,6 +25,7 @@ def test_chain_radius_of_gyration_stable(universe_single_state):
     """Protein chains should not explode or collapse due to PBC errors
     after applying alignment transformations."""
     protein = universe_single_state.select_atoms("protein and name CA")
+    protein.guess_bonds()
     apply_transformations.apply_complex_alignment_transformations(universe_single_state, protein)
 
     chain = protein.segments[0].atoms
@@ -91,6 +92,7 @@ def test_multichain_rmsd_shifting(simulation_skipped_nc, hybrid_system_skipped_p
 def test_rmsd_reference_is_first_frame(universe_single_state):
     """After alignment, RMSD at the first frame should be zero."""
     prot = universe_single_state.select_atoms("protein and name CA")
+    prot.guess_bonds()
     apply_transformations.apply_complex_alignment_transformations(
         universe_single_state, protein=prot
     )
@@ -113,6 +115,7 @@ def test_empty_protein_raises(universe_single_state):
 
 def test_atomgroup_as_ligands_raises(universe_single_state):
     prot = universe_single_state.select_atoms("protein and name CA")
+    prot.guess_bonds()
     lig = universe_single_state.select_atoms("resname UNK")
     with pytest.raises(TypeError, match="list of AtomGroups"):
         apply_transformations.apply_complex_alignment_transformations(
